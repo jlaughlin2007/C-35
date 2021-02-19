1c9ea52d86a6c779c56035bdf38e509da96f8e2f
jlaughlin2007 <71233108+jlaughlin2007@users.noreply.github.com>
2021-02-18 16:40:45 -1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var ball_1,database,data;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    ball_1 = createSprite(250,250,10,10);
+    ball_1.shapeColor = "red";
+    var ball_1pos = database.ref('ball/position');
+    ball_1pos.on("value",readPosition,showError);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+function readPosition(data){
+    position=data.val();
+    ball_1.x=position.x;
+    ball_1.y=position.y;
+    
+}
+
+function changePosition(x,y){
+    database.ref('ball/position').set({
+   'x':position.x+x,
+   
+   'y':position.y+y
+    })
+}
+
+function showError(){
+    console.log("noError")
+}
